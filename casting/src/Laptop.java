886230a4e3c64daecbe7f49d2066b47d45c1bd33
sakshi arun kasbe <sakshikasbe1001@gmail.com>
2023-02-01 21:48:17 +0530

    update casting

---
+++ casting/src/Laptop.java
@@ -0,0 +1,8 @@
+
+    public class Laptop extends Machine {
+        void displayDocument() {
+            System.out.println("Laptop is Displaying document");
+        }
+    }
+
+
---
+++ casting/src/Machine.java
@@ -0,0 +1,11 @@
+
+
+public class Machine {
+    void powerOn() {
+        System.out.println("Power on Machine");
+    }
+
+    void powerOff() {
+        System.out.println("Power Off Machine");
+    }
+}
