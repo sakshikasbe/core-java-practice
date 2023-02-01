353a8d6245f02ffcf7bbeb9c3901ae0df6cf9ed4
sakshi arun kasbe <sakshikasbe1001@gmail.com>
2023-02-01 21:24:23 +0530

    New file

---
+++ abstraction-with-classes/src/features/Shape.java
@@ -0,0 +1,22 @@
+package features;
+
+/**
+ * This class {@code Shape} is an abstract class to demonstrate partial abstraction
+ * and features of abstract class.
+ *
+ * @author Vikram Gupta
+ */
+public abstract class Shape {
+
+    public static final float PI = 3.142f;
+    public float area;
+
+    public Shape() {
+    }
+
+    public void displayArea() {
+        System.out.println("Area of " + this.getClass().getName() + " is " + area);
+    }
+
+    public abstract void calculateArea();
+}
